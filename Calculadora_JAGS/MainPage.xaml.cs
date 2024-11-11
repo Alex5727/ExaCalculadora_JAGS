f958569c6cba1881685a7f1d7921bf1c7136c8b7
Alex5727 <alejandrogrjalva@gmail.com>
2024-11-11 14:09:44 -0700

    Agregar archivos de proyecto.

---
+++ Calculadora_JAGS/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculadora_JAGS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void suma(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(Numero1.Text);
+            int n2 = Convert.ToInt32(Numero2.Text);
+
+            int resultado = n1 + n2;
+
+            Respuesta.Text = resultado.ToString();
+        }
+
+        private void resta(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(Numero1.Text);
+            int n2 = Convert.ToInt32(Numero2.Text);
+
+            int resultado = n1 - n2;
+
+            Respuesta.Text = resultado.ToString();
+        }
+
+        private void multiplicacion(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(Numero1.Text);
+            int n2 = Convert.ToInt32(Numero2.Text);
+
+            int resultado = n1 * n2;
+
+            Respuesta.Text = resultado.ToString();
+        }
+
+        private void division(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(Numero1.Text);
+            int n2 = Convert.ToInt32(Numero2.Text);
+
+            int resultado = n1 / n2;
+
+            Respuesta.Text = resultado.ToString();
+        }
+
+    }
+}
